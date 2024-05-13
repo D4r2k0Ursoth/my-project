bf31d847a517db29499a791a3304552b6deede4b
D4r2k0Ursoth <alessandro-arce@hotmail.com>
2024-05-13 02:39:18 -0600

    delete bugs

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function MovieDetail({ movie, img1, img2,img3,text3,text4}) {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const trailerUrl = movie.trailerUrl; // Debes obtener la URL del trailer de tu API
-
-    const handlePlayButtonClick = () => {
-        setIsPlaying(true);
-        // Aquí abrir un modal o una ventana emergente para reproducir el video
-        // librería de reproductores de video como react-player
-        
-        console.log('Reproducir video...');
-    };
-
+function MovieDetail({ movie, img1, img2, img3, text3, text4 }) {
     return (
-        <div className=" relative p-7">
+        <div className="relative p-7">
             <div>
-                <img className='mr-2'src={`${img3}`} alt='event' />
+                <img className='mr-2' src={`${img3}`} alt='event' />
             </div>
             <section className='mt-32'>
-            <h2 className="select-none text-3xl font-bold mb-4">{movie.title}</h2>
+                <h2 className="select-none text-3xl font-bold mb-4">{movie.title}</h2>
                 <div className='flex'>
                     <div className='flex select-none'>
-                        <button onClick={handlePlayButtonClick} className="flex font-extrabold text-sm bg-white text-black px-5 py-2.5 rounded hover:bg-gray-200 hover:text-black">
+                        <button className="flex font-extrabold text-sm bg-white text-black px-5 py-2.5 rounded hover:bg-gray-200 hover:text-black">
                             <img className='mr-2 mt-1' src={`${img2}`} alt='event' />
                             <span>{text3}</span>
                         </button>
@@ -36,12 +25,12 @@ function MovieDetail({ movie, img1, img2,img3,text3,text4}) {
                             </button>
                         </div>
                     </div>
-            </div>
-            <section>
-            <div className='mt-4 w-[40%] '>
-                <p className="select-none text-ls font-ligth text-balance overflow-hidden line-clamp-3">{movie.overview}</p>
-            </div>
-            </section>
+                </div>
+                <section>
+                    <div className='mt-4 w-[40%]'>
+                        <p className="select-none text-ls font-ligth text-balance overflow-hidden line-clamp-3">{movie.overview}</p>
+                    </div>
+                </section>
             </section>
         </div>
     );
